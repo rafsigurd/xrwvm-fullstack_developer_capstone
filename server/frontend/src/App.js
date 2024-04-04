6bb1da43f618b3dd48ad5b7e5272e17e06ae3e74
rafsigurd <sigurdlim@outlook.com>
2024-04-04 12:00:28 -0400

    Modified multiple files regarding dealerships page

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,18 @@
 import LoginPanel from "./components/Login/Login"
-import SubmitPanel from "./components/Register/Register"
+import ReigsterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<SubmitPanel />} />
+      <Route path="/login" element={<LoginPanel/>} />
+      <Route path="/register" element={<ReigsterPanel/>}/>
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
